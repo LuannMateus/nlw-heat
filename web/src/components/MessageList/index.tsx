45609e9db732757a5200c96c76d652c62b79b93e
LuannMateus <luanmateuslima34@gmail.com>
2021-10-19 23:16:48 -0300

    React - Add backend integration getting the last three messages

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,51 +1,45 @@
+import { useEffect, useState } from 'react';
 import logoImg from '../../assets/logo.svg';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export const MessageList = () => {
+  const [messages, setMessages] = useState<Message[]>();
+
+  useEffect(() => {
+    api
+      .get<Message[]>('/messages/lastThree')
+      .then((res) => setMessages(res.data));
+  }, []);
+
   return (
     <section className={styles.messageListWrapper}>
       <img src={logoImg} alt="DOWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            DOWhile!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/LuannMateus.png" alt="Luan Mateus" />
-            </div>
-            <span>Luan Mateus</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            DOWhile!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/LuannMateus.png" alt="Luan Mateus" />
-            </div>
-            <span>Luan Mateus</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            DOWhile!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/LuannMateus.png" alt="Luan Mateus" />
-            </div>
-            <span>Luan Mateus</span>
-          </div>
-        </li>
+        {messages?.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
