5f07c4e382c8f20022c2225c126c58e8001b3cf7
LuannMateus <luanmateuslima34@gmail.com>
2021-10-21 09:05:02 -0300

    React Native - Add Message component and MessageList

---
+++ nlwheatapp/src/components/MessageList/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Message } from '../Message';
+import { MESSAGES_EXAMPLE } from '../../utils/messages';
+import { styles } from './styles';
+
+export const MessageList = () => {
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      {MESSAGES_EXAMPLE.map((message) => {
+        return <Message key={message.id} data={message} />;
+      })}
+    </ScrollView>
+  );
+};
